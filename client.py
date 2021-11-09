696e068cfaf4c06bbcc144fe9a5d2dd79eeb3701
msr8 <live.shashwat@gmail.com>
2021-11-09 12:09:54 +0530

    barebone done

--- client.py
+++ client.py
@@ -80,15 +80,6 @@ elif signup_or_login in ['2','signup']:
 		if not username.isalnum():
 			print(f'{Fore.RED}{Style.BRIGHT}ERROR: Please make sure that the username only contains alphabets and numbers{Style.RESET_ALL}\n')
 			continue
-		# Checks if username already exists
-		try:
-			if bool( rq.get(BASE_URL+f'exists/{username}').json()['exists'] ):
-				print(f'{Fore.RED}{Style.BRIGHT}ERROR: Username not available{Style.RESET_ALL}\n')
-				continue
-		# If the site is down or we dont have a connection
-		except rq.exceptions.ConnectionError:
-			print(f'{Fore.RED}{Style.BRIGHT}FATAL: Either the server is down, or you don\'t have an internet connection, please try again later{Style.RESET_ALL}\n')
-			sys.exit()
 		# Checks if the password is between 3 and 20 chars long
 		if not (len(password) >= 3 and len(password) <= 20):
 			print(f'{Fore.RED}{Style.BRIGHT}ERROR: Please make sure that the password is atleast 3 characters long and at maximum, 20 characters long{Style.RESET_ALL}\n')
@@ -101,6 +92,15 @@ elif signup_or_login in ['2','signup']:
 		if not password == password_confirm:
 			print(f'{Fore.RED}{Style.BRIGHT}ERROR: Passwords do not match, please make sure you type the same password both the times{Style.RESET_ALL}\n')
 			continue
+		# Checks if username already exists
+		try:
+			if bool( rq.get(BASE_URL+f'exists/{username}').json()['exists'] ):
+				print(f'{Fore.RED}{Style.BRIGHT}ERROR: Username not available{Style.RESET_ALL}\n')
+				continue
+		# If the site is down or we dont have a connection
+		except rq.exceptions.ConnectionError:
+			print(f'{Fore.RED}{Style.BRIGHT}FATAL: Either the server is down, or you don\'t have an internet connection, please try again later{Style.RESET_ALL}\n')
+			sys.exit()
 		
 		# Send req
 		try:
@@ -172,7 +172,7 @@ elif signup_or_login in ['1','login']:
 
 
 
-print(AUTH)
+
 
 
 
@@ -185,23 +185,172 @@ while True:
 	print(f'(Logged in as @{AUTH["username"]})\n\n')
 	print(f'{Style.BRIGHT}Where would you like to go?{Style.RESET_ALL}\n')
 	print(f'{Fore.CYAN}[1]{Fore.RESET} {Fore.YELLOW}Inbox{Fore.RESET}')
-	print(f'{Fore.CYAN}[2]{Fore.RESET} {Fore.YELLOW}Send a message{Fore.RESET}')
+	print(f'{Fore.CYAN}[2]{Fore.RESET} {Fore.YELLOW}Compose a mail{Fore.RESET}')
 	print(f'{Fore.CYAN}[3]{Fore.RESET} {Fore.YELLOW}Exit{Fore.RESET}')
 	home_chc = input('\n').lower()
-	if not home_chc in ['1','2','3','inbox','send a message','exit']:
-		cls()
+
+	cls()
+	if not home_chc in ['1','2','3','inbox','compose a mail','exit']:
 		print(f'{Fore.RED}{Style.BRIGHT}ERROR: Please select a valid option{Style.RESET_ALL}\n')
 		continue
-	break
+	if home_chc in ['3','exit']:
+		sys.exit()
 
 
-print(AUTH)
+
+
+
+
+
 
 
 
 
 # ---------------------------------------------------------------------------------------------------- SEND A MSG ----------------------------------------------------------------------------------------------------
 
+	if home_chc in ['2','compose a mail']:
+
+		print(f'{Style.BRIGHT}From:{Style.RESET_ALL} @{AUTH["username"]}\n')
+		destination = input(f'{Style.BRIGHT}To:{Style.RESET_ALL} @').lower()
+		
+		# Checks if the destination and origin is same
+		if destination == AUTH['username']:
+			cls()
+			print(f'{Fore.RED}{Style.BRIGHT}ERROR: You cannot send messages to yourself{Style.RESET_ALL}\n')
+			continue
+
+		# Checks if destination exists
+		try:
+			if not bool( rq.get(BASE_URL+f'exists/{destination}').json()['exists'] ):
+				cls()
+				print(f'{Fore.RED}{Style.BRIGHT}ERROR: @{destination} does NOT exist. Please recheck your spelling{Style.RESET_ALL}\n')
+				continue
+		# If the site is down or we dont have a connection
+		except rq.exceptions.ConnectionError:
+			print(f'{Fore.RED}{Style.BRIGHT}FATAL: Either the server is down, or you don\'t have an internet connection, please try again later{Style.RESET_ALL}\n')
+			sys.exit()
+
+		print('\n(Please make sure that the header doesn\'t exceed 30 characters or else it will not show to the recipent. Press enter to leave it blank)')
+		mail_head = input(f'{Style.BRIGHT}Header:{Style.RESET_ALL} ')
+		mail_body = input(f'\n{Style.BRIGHT}Please write the body of the mail:{Style.RESET_ALL}\n\n')
+
+		cls()
+
+		# Sends req
+		try:
+			body = AUTH.copy()
+			body['destination'] = destination
+			body['header'] = mail_head
+			body['body'] = mail_body
+			response = rq.post(BASE_URL+'message', body).json()
+
+			# Checks if there was an error
+			if response.get('error'):
+				print(f'{Fore.RED}{Style.BRIGHT}SENDING MESSAGE ERROR: {response["error"]}{Style.RESET_ALL}\n')
+				continue
+		# If the site is down or we dont have a connection
+		except rq.exceptions.ConnectionError:
+			print(f'{Fore.RED}{Style.BRIGHT}FATAL: Either the server is down, or you don\'t have an internet connection, please try again later{Style.RESET_ALL}\n')
+			sys.exit()
+
+		# Checks if it was a success
+		if response.get('status') == 'success':
+			cls()
+			print('(Message was succesfully sent)')
+
+
+
+
+		
+
+
+
+
+
+
+
+
+
+
+# ---------------------------------------------------------------------------------------------------- INBOX ----------------------------------------------------------------------------------------------------
+
+	elif home_chc in ['1','inbox']:
+		in_inbox = True
+		while in_inbox:
+			to_stop_inbox = False
+
+			# Asks the server for our inbox
+			try:
+				body = AUTH.copy()
+				msges = rq.post(BASE_URL+'inbox', body).json()			
+				# Checks if there was an error
+				if msges.get('error'):
+					print(f'{Fore.RED}{Style.BRIGHT}SENDING MESSAGE ERROR: {msges["error"]}{Style.RESET_ALL}\n')
+					continue		
+			# If the site is down or we dont have a connection
+			except rq.exceptions.ConnectionError:
+				print(f'{Fore.RED}{Style.BRIGHT}FATAL: Either the server is down, or you don\'t have an internet connection, please try again later{Style.RESET_ALL}\n')
+				sys.exit()
+			
+			# Checks if we dont have any messages
+			if not len(msges):
+				print(f'{Style.BRIGHT}There are no unread mails. Please press enter to continue\n{Style.RESET_ALL}')
+				input()
+				cls()
+				in_inbox = False
+				continue
+
+			print(f'{Fore.RED}{Style.BRIGHT}WARNING: Mails will be DELETED once you open them{Style.RESET_ALL}\n')
+			print(f'{Back.BLACK}{Fore.WHITE}{Style.BRIGHT}S. NO{Style.RESET_ALL}    {Back.BLACK}{Fore.WHITE}{Style.BRIGHT}From{Style.RESET_ALL}    {Back.BLACK}{Fore.WHITE}{Style.BRIGHT}Header{Style.RESET_ALL}\n')
+
+			# Prints the data
+			msg_ids = list(msges.keys())
+			for msg_id in msges.keys():
+				dic = msges[msg_id]
+				print(f'{Back.BLACK}{Fore.WHITE}{msg_id}{Style.RESET_ALL}    {Back.BLACK}{Fore.WHITE}{dic["origin"]}{Style.RESET_ALL}    {Back.BLACK}{Fore.WHITE}{dic["header"]}{Style.RESET_ALL}')
+
+			# Gets the id of the message that the user wants to see
+			valid_msg_id = False
+			while not valid_msg_id:
+				inbox_chc = input(f'\n{Style.BRIGHT}Please enter the ID of the mail you want to view or press {Style.RESET_ALL}{Fore.YELLOW}e{Style.RESET_ALL}{Style.BRIGHT} to exit your inbox{Style.RESET_ALL}\n')
+				if inbox_chc in msg_ids:
+					valid_msg_id = True
+				elif inbox_chc == 'e':
+					valid_msg_id = True
+					to_stop_inbox = True
+				else:
+					print(f'{Style.BRIGHT}{Fore.RED}ERROR: Invalid message ID{Style.RESET_ALL}\n')
+
+			# Checks if user wanted to exit inbox
+			if to_stop_inbox:
+				cls()
+				in_inbox = False
+				continue
+
+			# Gets the message
+			try:
+				body = AUTH.copy()
+				body['message_id'] = inbox_chc
+				msg = rq.post(BASE_URL+'read', body).json()			
+				# Checks if there was an error
+				if msges.get('error'):
+					print(f'{Fore.RED}{Style.BRIGHT}SENDING MESSAGE ERROR: {msges["error"]}{Style.RESET_ALL}\n')
+					continue		
+			# If the site is down or we dont have a connection
+			except rq.exceptions.ConnectionError:
+				print(f'{Fore.RED}{Style.BRIGHT}FATAL: Either the server is down, or you don\'t have an internet connection, please try again later{Style.RESET_ALL}\n')
+				sys.exit()
+
+
+			# Prints the mail
+			cls()
+			print('(Press enter to continue)')
+			print(f'{Style.BRIGHT}FROM:{Style.RESET_ALL} @{msg["origin"]}\n')
+			print(f'{Style.BRIGHT}HEADER:{Style.RESET_ALL} {msg["header"]}\n')
+			print(f'{Style.BRIGHT}BODY:{Style.RESET_ALL}\n\n{msg["body"]}\n')
+			input()
+			cls()
+			continue
 
 
 
@@ -216,13 +365,12 @@ print(AUTH)
 
 
 
-'''
-MESSAGING PLAN
-Check auth
-check user exists
-make json if not made
-add msg
-'''
+
+
+
+
+
+
 
 '''
 Fore: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
---
+++ server.py
@@ -0,0 +1,242 @@
+from flask_restful import Api, Resource
+from flask import Flask, request
+import sqlite3
+import json
+import os
+
+DATA = os.path.join( os.path.dirname(__file__) , 'DATA' )
+INBOX = os.path.join( DATA , 'INBOX' )
+USERNAMES = os.path.join( DATA , 'usernames.db' )
+MSG_COUNT = os.path.join( DATA , 'messagecount.txt' )
+
+
+if not os.path.exists(DATA):
+	os.makedirs(DATA)
+if not os.path.exists(INBOX):
+	os.makedirs(INBOX)
+
+def init_database():
+	try:
+		db_connection = sqlite3.connect(USERNAMES)
+		crsr = db_connection.cursor()
+		cmd = 'CREATE TABLE usernames ( username VARCHAR(20) PRIMARY KEY, password VARCHAR(20) )'
+		crsr.execute(cmd)
+		db_connection.close()
+	except sqlite3.OperationalError:
+		pass
+
+def get_msg_count():
+	with open(MSG_COUNT) as f:
+		ret = len(f.readlines())
+	return ret
+
+def check_pass(username, password):
+	conn = sqlite3.connect(USERNAMES)
+	crsr = conn.cursor()
+	cmd = f'SELECT * FROM usernames WHERE username=="{username}" and password=="{password}"'
+	crsr.execute(cmd)
+	data = crsr.fetchall()
+	conn.close()
+	# Checks if the data is of any length
+	if not len(data):
+		return False
+	# Checks if username and password is correct (to counter exploits)
+	ret = True if data[0][1] == password else False
+	return ret
+
+
+
+init_database()
+
+
+
+
+
+
+
+
+
+app = Flask(__name__)
+api = Api(app)
+
+signup_args = []
+
+class HelloWorld(Resource):
+	def get(self, name):
+		return {'text':'Hello' , 'param':name}
+
+class Exists(Resource):
+	def get(self, username):
+		# Checks if the username exists
+		db_connection = sqlite3.connect(USERNAMES)
+		crsr = db_connection.cursor()
+		crsr.execute(f'SELECT * FROM usernames WHERE username=="{username}"')
+		ans = True if len(crsr.fetchall()) else False
+		db_connection.close()
+		# Returns the data
+		return {'username':username , 'exists':ans}
+
+
+class Signup(Resource):
+	def post(self):
+		try:
+			# Gets the data given to us
+			data = request.form
+			username = data['username']
+			password = data['password']
+			# Executes the command in SQL
+			conn = sqlite3.connect(USERNAMES)
+			crsr = conn.cursor()
+			cmd = f'INSERT INTO usernames VALUES ("{username}" , "{password}")'
+			crsr.execute(cmd)
+			conn.commit()
+			conn.close()
+			# Tells that it was a success
+			return {'username':username , 'password':password , 'status':'success'}
+		# In case of exception, tells what exception
+		except Exception as e:
+			print(e)
+			return {'error':str(e)}
+
+
+class Login(Resource):
+	def post(self):
+		try:
+			# Gets the data given to us
+			data = request.form
+			username = data['username']
+			password = data['password']
+			matching = check_pass(username, password)
+			return {'username':username , 'password':password , 'matching':matching}
+		# In case of exception, tells what exception
+		except Exception as e:
+			return {'error':str(e)}
+
+
+class Message(Resource):
+	def post(self):
+		try:
+			# Gets the data given to us
+			data = request.form
+			username = data['username']
+			password = data['password']
+			# Checks if username and pass are correct
+			if not check_pass(username, password):
+				return {'error': 'Stop trying to spoof messages -_-'}
+			destination = data['destination'].lower()
+			# Check if they're teying to send msges to themself
+			if destination == username:
+				return {'error': 'You cannot send messages to yourself!'}
+			mail_head = data['header'] if len(data['header']) <= 30 else '-'
+			mail_body = data['body']
+
+			# Gets the message id
+			msg_id = str(get_msg_count() + 1)
+
+			# Checks if the reciever json exists and gets the data
+			recv_json = os.path.join(INBOX,f'{destination}.json')
+			if os.path.isfile(recv_json):
+				with open(recv_json) as rf:
+					prev_msges = json.load(rf)
+			else:
+				prev_msges = {}
+
+			# Adds the message
+			prev_msges[msg_id] = {'origin':username , 'header':mail_head , 'body':mail_body}
+			# Saves it
+			with open(recv_json, 'w') as wf:
+				json.dump(prev_msges, wf, indent=2)
+			# Adds 1 to the msg id
+			with open(MSG_COUNT, 'a') as f:
+				f.write('-\n')
+
+			return {'status':'success'}
+
+		except Exception as e:
+			return {'error': str(e)}
+
+
+class Inbox(Resource):
+	def post(self):
+		try:
+			# Gets the data given to us
+			data = request.form
+			username = data['username']
+			password = data['password']
+			if not check_pass(username, password):
+				return {'error': 'Stop trying to snoop messages -_-'}
+
+			# Gets the messages
+			json_file = os.path.join(INBOX,f'{username}.json')
+			# If file not exists, sends an empty dict
+			if not os.path.isfile(json_file):
+				return {}
+			with open(json_file) as f:
+				msges = json.load(f)
+
+			# Deletes the bodies
+			for key in msges.keys():
+				del msges[key]['body']
+
+			# Returns the dict with bodies deletes
+			return msges
+
+		except Exception as e:
+			return {'error': str(e)}
+
+
+class Read(Resource):
+	def post(self):
+		try:
+			# Gets the data given to us
+			data = request.form
+			username = data['username']
+			password = data['password']
+			msg_id = data['message_id']
+
+			if not check_pass(username, password):
+				return {'error': 'Stop trying to snoop messages -_-'}
+
+			# Gets the messages
+			json_file = os.path.join(INBOX,f'{username}.json')
+			with open(json_file) as rf:
+				msges = json.load(rf)
+
+			# Checks if message is in the inbox
+			if not msg_id in msges.keys():
+				return {'error': 'The message you are trying to access is not present'}
+
+			# Gets the message and deletes it from the json file
+			msg_dic = msges[msg_id]
+			del msges[msg_id]
+			with open(json_file,'w') as wf:
+				json.dump(msges, wf, indent=2)
+
+			# Returns the message
+			return msg_dic
+
+		except Exception as e:
+			return {'error': str(e)}
+
+
+
+
+
+
+
+
+# Adds the link and make it accesible
+api.add_resource(HelloWorld, '/hello/<string:name>')
+api.add_resource(Exists, '/exists/<string:username>')
+api.add_resource(Signup, '/signup')
+api.add_resource(Login, '/login')
+api.add_resource(Message, '/message')
+api.add_resource(Inbox, '/inbox')
+api.add_resource(Read, '/read')
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
+
