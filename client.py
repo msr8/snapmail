07ad757dbccaa42b702d3e6acdb6a657f0bd3c90
msr8 <live.shashwat@gmail.com>
2022-03-23 16:21:03 +0530

    removed some lines in client

--- client.py
+++ client.py
@@ -457,38 +457,3 @@ while True:
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-Fore: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
-Back: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
-Style: DIM, NORMAL, BRIGHT, RESET_ALL
-'''
-
-
-
